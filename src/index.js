074a7b1c120cedb4f4396f25c342ea9bfdc28881
Cao Xiaoqi <xqcao@thoughtworks.com>
2018-09-10 00:43:56 +0800

    Add ci & eslint & webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-export default from './js/HelloWorld';
+import './index.scss';
+import 'babel-polyfill';
+import HelloPage from './pages/HelloPage';
+
+document.getElementById('app').append(new HelloPage().render());
