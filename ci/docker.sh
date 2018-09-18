06a9acdd2b240f182cdc91cebaf541f3ac3d8e3f
Cao Xiaoqi <xqcao@thoughtworks.com>
2018-09-18 23:37:45 +0800

    Update docker to build docker image to docker hub

--- ci/docker.sh
+++ ci/docker.sh
@@ -1,6 +1,22 @@
 #! bin/bash
 set -e -u
 
-CURRENT_VERSION=`cat package.json | jq -r .version`
+REPOSITORY = xqstencils/node-jest-server
+VERSION=`cat package.json | jq -r .version`
 
 echo `env`
+
+echo 'Login to docker registry'
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+
+echo 'Build docker image'
+docker build -t ${REPOSITORY} .
+
+echo 'Tag image'
+docker tag ${REPOSITORY}:latest ${REPOSITORY}:${VERSION}
+
+echo 'Push image'
+docker push ${REPOSITORY}:latest
+docker push ${REPOSITORY}:${VERSION}
+
+echo "Build and push docker image ${REPOSITORY}:${VERSION} to docker registry"
