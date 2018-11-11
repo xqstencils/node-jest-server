efa92aba6d234fc26718e74a6b655f991daeef31
Cao Xiaoqi <xqcao@thoughtworks.com>
2018-11-11 23:03:28 +0800

    Upgrade dependencies & style

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  rootDir: '../src',
+  rootDir: '../',
   testURL: 'http://localhost/',
-  globals: {
-  },
-  coverageDirectory: '../coverage',
-  collectCoverageFrom: [
-    '**/*.{js,jsx}'
-  ],
-  coverageReporters: ['html'],
-  testMatch: ['**/__tests__/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  globals: { },
+  testMatch: ['<rootDir>/src/**/__tests__/*.js', '<rootDir>/src/**/?(*.)+(spec|test).js'],
   testPathIgnorePatterns: [
     '<rootDir>/src/index.js'
   ],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['html'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.js'
+  ],
   coveragePathIgnorePatterns: [
+    '<rootDir>/src/index.js',
     '<rootDir>/dist',
     '<rootDir>/coverage',
     '<rootDir>/node_modules'
   ],
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/__init__/fileMock.js'
   }
 }
